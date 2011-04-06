d77f1280e53b2f7c4b58e647dfb43fa8a325b773
Romain Bertrand <romain@doxin.net>
2011-04-06 16:30:22 +0200

    better unified deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+HOMEDIR=`pwd`
+
+echo "Deploying site ..."
 cd site && sh deploy.sh
-cd -
+cd $HOMEDIR
+
+echo "Deploying blog ..."
 cd blog && sh deploy.sh
-cd -
+cd $HOMEDIR
+
+echo "Deploying tech ..."
 cd tech && sh deploy.sh
-cd -
+cd $HOMEDIR
--- site/deploy.sh
+++ site/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 doxite -sd src -dd /var/www/doxin.net/site -se md -o markdown
+echo "Done."
